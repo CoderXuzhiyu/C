ca2c79285af9493444089fd5d3d629c268670caa
智禹 许 <1242064152@qq.com>
2022-08-17 14:21:23 +0800

    j

--- Project4/源.cpp
+++ Project4/源.cpp
@@ -547,25 +547,88 @@
 //	}
 //	return 0;
 //}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int d = 0;
+//	int x = 0;
+//	int l = 0;
+//	int p = 0;
+//	int k = 0;
+//
+//
+//	int v = 0;
+//	int g = 0;
+//	int y = 0;
+//
+//	int j = 0;
+//	int u = 0;
+//	int t = 0;
+//	return 0;
+//}
+//int get_max(int x, int y)
+//{
+//	int z = 0;
+//	if (x > y)
+//	{
+//		z = x;
+//	}
+//	else z = y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = get_max(a, b);
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+
+//函数返回类型的地方写成：void，表示这个函数不返回任何值，也不需要返回。
+
+//void Swap(int* pa, int* pb)
+//{
+//	int z = 0;
+//	z = *pa;
+//	*pa = *pb;
+//    *pb= z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("交换前：a=%d,b=%d\n", a,b);
+//	Swap(&a, &b);
+//	printf("交换后: a=%d,b=%d\n", a,b);
+//	return 0;
+//}
+int prime(int n)
+{
+	int j = 0;
+	for (j = 2; j <=sqrt(n); j++)
+	{
+		if (n % j == 0)
+			return 1;
+	}
+	return 0;
+}
 int main()
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
-	int d = 0;
-	int x = 0;
-	int l = 0;
-	int p = 0;
-	int k = 0;
+	int count=0;
+	int i;
+	for (i = 100; i <= 200; i++)
+	{
+		if (prime(i) == 0)
+		{
+			printf("%d ", i);
+			count++;
+		}
 
-
-	int v = 0;
-	int g = 0;
-	int y = 0;
-
-	int j = 0;
-	int u = 0;
-	int t = 0;
+	}
+	printf("\ncount=%d\n", count);
 	return 0;
 }
