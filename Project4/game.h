f71ca89617e451bb78c1dffc890cda7d0163e11b
智禹 许 <1242064152@qq.com>
2022-08-23 20:42:22 +0800

    8/23

---
+++ Project4/game.h
@@ -0,0 +1,6 @@
+//头文件的包含
+#include<stdio.h>
+
+//符号的定义
+#define row 3
+#define col 3
--- Project4/源.cpp
+++ Project4/源.cpp
@@ -1176,26 +1176,26 @@ void menu()
 	printf("*****  0.exit  ********\n");
 	printf("***********************\n");
 }
-int main()
-{
-	int input = 0;
-	do
-	{
-		menu();
-		printf("请选择：");
-		scanf_s("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("游戏结束\n");
-			break;
-		default:
-			printf("选择错误，请重新选择\n");
-			break;
-		}
-	} while (input);
-	return 0;
-}
+////int main()
+////{
+////	int input = 0;
+////	do
+////	{
+////		menu();
+////		printf("请选择：");
+////		scanf_s("%d", &input);
+////		switch (input)
+////		{
+////		case 1:
+////			game();
+////			break;
+////		case 0:
+//			printf("游戏结束\n");
+//			break;
+//		default:
+//			printf("选择错误，请重新选择\n");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
