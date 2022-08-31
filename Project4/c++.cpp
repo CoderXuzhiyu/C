6a5090f6119577aaeafdc1414fc2d852f9b228ae
智禹 许 <1242064152@qq.com>
2022-08-31 22:00:58 +0800

    8/31

--- Project4/c++.cpp
+++ Project4/c++.cpp
@@ -1,45 +1,99 @@
 #include <iostream>
 using namespace std;
 #include<string>
+//int main()
+//{
+//	/*cout << "hello world"<< endl;
+//	system("pause");*/
+//	/*cout << "" << sizeof(short) << endl;
+//	cout << "" << sizeof(int) << endl;
+//	cout << "" << sizeof(long) << endl;
+//	cout << "" << sizeof(long long) << endl;
+//	short num1 = 10;
+//	cout << "" << sizeof(num1) << endl;*/
+//	//float f1 = 3.14f;
+//	//cout << "" << sizeof(float) << endl; 
+//	//cout << "" << sizeof(double) << endl; 
+//	//float f2 = 3e2;
+//	//float f3 = 3e-2;
+//	/*char ch = 'a';
+//	char ch1[] = "abcde";
+//	cout << ch << endl;
+//	cout << (int)ch << endl;
+//	cout << "hello world\n" ;
+//	cout << "\\\\";*/
+//	/*string str2 = "hello world";
+//	cout << str2 << endl;*/
+//	/*bool flag = true;
+//	flag = false;
+//	cout << flag << endl;
+//	cout << sizeof(bool) << endl;*/
+//	/*int a = 0;
+//	cout << "请给整形变量赋值：" << endl;
+//	cin >> a;*/
+//	//char ch = 'a';
+//	//cout << "赋值" << endl;
+//	//cin >> ch;
+//	//cout << "" << ch << endl;
+//	/*string str;
+//	cin >> str;
+//	cout << "" << str << endl;*/
+//	//bool flag = false;
+//	//cin >> flag;
+//	//cout << "" << flag << endl;
+//	//cout << (1 != 2) << endl;
+//	//int a = 0;
+//	//int b = 0;
+//	////cout << !a << endl;
+//	///*cout << !!a << endl;*/
+//	//cout << (a && b) << endl;
+//	//cout << (a || b) << endl;
+//	int a = 30;
+//	int b = 20;
+//	int c = 0;
+//	c = a > b ? a : b;
+//	cout << c << endl;
+//	switch (c)
+//	{
+//	case 20:
+//		cout << "c=" << c << endl;
+//		break;
+//	case 30:
+//		cout << "c=" << c << endl;
+//		break;
+//	default:
+//		break;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	//cout << "1.xxxx" << endl;
+//	//cout << "2.xxxx" << endl;
+//	//goto flag;
+//	//cout << "3.xxxx" << endl;
+//	//cout << "4.xxxx" << endl;
+//	//flag:
+//	//cout << "5.xxxx" << endl;
+//	return 0;
+//}
 int main()
 {
-	/*cout << "hello world"<< endl;
-	system("pause");*/
-	/*cout << "" << sizeof(short) << endl;
-	cout << "" << sizeof(int) << endl;
-	cout << "" << sizeof(long) << endl;
-	cout << "" << sizeof(long long) << endl;
-	short num1 = 10;
-	cout << "" << sizeof(num1) << endl;*/
-	//float f1 = 3.14f;
-	//cout << "" << sizeof(float) << endl; 
-	//cout << "" << sizeof(double) << endl; 
-	//float f2 = 3e2;
-	//float f3 = 3e-2;
-	/*char ch = 'a';
-	char ch1[] = "abcde";
-	cout << ch << endl;
-	cout << (int)ch << endl;
-	cout << "hello world\n" ;
-	cout << "\\\\";*/
-	/*string str2 = "hello world";
-	cout << str2 << endl;*/
-	/*bool flag = true;
-	flag = false;
-	cout << flag << endl;
-	cout << sizeof(bool) << endl;*/
-	/*int a = 0;
-	cout << "请给整形变量赋值：" << endl;
-	cin >> a;*/
-	//char ch = 'a';
-	//cout << "赋值" << endl;
-	//cin >> ch;
-	//cout << "" << ch << endl;
-	/*string str;
-	cin >> str;
-	cout << "" << str << endl;*/
-	bool flag = false;
-	cin >> flag;
-	cout << "" << flag << endl;
+	//int arr[5];
+	//arr[1] = 10;
+	//arr[2] = 1;
+	//arr[3] = 1;
+	//arr[4] = 1;
+	//arr[0] = 1;
+	//cout << arr[1] << endl;
+	//int arr2[5] = { 10,20,30,40,50 };
+	//int arr3[] = { 10,20,30,40,50,60 };
+	//cout << sizeof(arr) / sizeof(arr[0]) << endl;
+	//cout << (int)arr << endl;
+	//cout << (int)&arr[0] << endl;
+	int arr4[2][2] = { 0,1,2,3 };
+	cout << sizeof(arr4) << endl;
+	cout << &arr4 << endl;
+	
 	return 0;
 }
